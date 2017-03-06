f0f3e93b72579143fb3886ff8962c4972bb0377c
Kameron Black <kameron.black@uvu.edu>
2017-03-06 12:08:54 -0700

    Initial prototyping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float maxSpeed = 6.0f;
+	public float moveHorizontal;
+	public float moveVertical;
+	private Rigidbody2D body;
+
+	void Awake() {
+		body = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate() {
+		body.velocity = new Vector2(moveHorizontal * maxSpeed, moveVertical * maxSpeed);
+	}
+
+	void Update() {
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
 	}
 }
